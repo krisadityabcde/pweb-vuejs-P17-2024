cac136e2c008b748280613e2fee7ab41772ce684
RM. Novian Malcolm Bayuputra <122516105+rmnovianmalcolmb@users.noreply.github.com>
2024-11-27 11:06:41 +0700

    Update HomeView.vue

--- pweb-vuejs-P17-2024/src/views/HomeView.vue
+++ pweb-vuejs-P17-2024/src/views/HomeView.vue
@@ -1,9 +1,57 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script lang="ts">
+import BookCard from "../components/BookCard.vue";
+
+interface BookObject {
+  _id: string;
+  title: string;
+  author: string;
+  tags: string[];
+  publishedDate: string;
+  initialQty: number;
+  qty: number;
+  publisher: string;
+}
+
+export default {
+  name: "HomeView",
+  data: () => ({
+    booksData: [] as BookObject[],
+    fetchError: false,
+  }),
+  async mounted() {
+    try {
+      const response = await fetch("http://localhost:3000/books");
+      if (!response.ok) {
+        throw new Error("Failed to fetch books data");
+      }
+      const data = await response.json();
+      this.booksData = [...data.data];
+    } catch (error) {
+      console.error(error);
+      this.fetchError = true;
+    }
+  },
+  components: {
+    BookCard,
+  },
+};
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="mt-10 mx-8 pb-24">
+    <h1 class="font-bold text-3xl text-center">Book Gallery App P10</h1>
+    <div class="flex flex-wrap justify-center gap-4 mt-10">
+      <BookCard
+        v-if="booksData.length"
+        v-for="book in booksData"
+        :key="book._id"
+        :book="book"
+        class="w-full sm:w-full md:w-2/6 lg:w-1/4"
+      />
+      <h1 class="font-bold text-3xl text-center w-full" v-else-if="fetchError">
+        Failed to load books data
+      </h1>
+      <h1 class="font-bold text-3xl text-center w-full" v-else>Loading...</h1>
+    </div>
   </main>
 </template>
