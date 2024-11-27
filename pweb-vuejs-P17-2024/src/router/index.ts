cef9b81d70ac929b1c5e711d41a12c9d13ccfd28
RM. Novian Malcolm Bayuputra <122516105+rmnovianmalcolmb@users.noreply.github.com>
2024-11-27 11:09:21 +0700

    Update index.ts

--- pweb-vuejs-P17-2024/src/router/index.ts
+++ pweb-vuejs-P17-2024/src/router/index.ts
@@ -1,23 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AddBookView from "@/views/AddBookView.vue";
+import DetailBookView from "@/views/DetailBookView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: "/add-book",
+      name: "add-book",
+      component: AddBookView,
+    },
+    {
+      path: "/detail/:id",
+      name: "book detail",
+      component: DetailBookView,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "PageNotFound",
+      redirect(_) {
+        return { path: "/" };
+      },
     },
   ],
-})
+});
 
-export default router
+export default router;
