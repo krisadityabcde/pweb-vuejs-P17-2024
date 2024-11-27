f364b14765d42e0fe3843d2df4c50f7e6564fd6f
RM. Novian Malcolm Bayuputra <122516105+rmnovianmalcolmb@users.noreply.github.com>
2024-11-27 11:11:16 +0700

    Update App.vue

--- pweb-vuejs-P17-2024/src/App.vue
+++ pweb-vuejs-P17-2024/src/App.vue
@@ -1,85 +1,93 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+    closeMenu() {
+      this.isMenuOpen = false;
+    },
+  },
+  components: {
+    RouterLink,
+    RouterView,
+  },
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <nav class="bg-gray-800 p-4">
+    <div class="container mx-auto flex justify-between items-center">
+      <div class="flex items-center space-x-4">
+        <h3 class="text-white font-bold">Book App PWEB A | P10 🗿</h3>
+      </div>
+      <div class="md:hidden">
+        <button @click="toggleMenu" class="text-white focus:outline-none">
+          <svg
+            class="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <div class="hidden md:flex items-center space-x-4">
+        <RouterLink to="/" class="text-white">Home</RouterLink>
+        <RouterLink to="/add-book" class="text-white">Add New Book</RouterLink>
+      </div>
     </div>
-  </header>
+  </nav>
+
+  <div
+    :class="{
+      'translate-x-full': !isMenuOpen,
+      'translate-x-0': isMenuOpen,
+    }"
+    class="fixed top-0 right-0 h-full w-64 bg-gray-800 text-white transform transition-transform duration-300 ease-in-out md:hidden"
+  >
+    <button
+      @click="closeMenu"
+      class="absolute top-4 right-4 text-white focus:outline-none"
+    >
+      <svg
+        class="w-6 h-6"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M6 18L18 6M6 6l12 12"
+        ></path>
+      </svg>
+    </button>
+    <div class="mt-16 flex flex-col items-center space-y-4">
+      <RouterLink to="/" class="text-white" @click="closeMenu">Home</RouterLink>
+      <RouterLink to="/add-book" class="text-white" @click="closeMenu"
+        >Add New Book</RouterLink
+      >
+    </div>
+  </div>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
